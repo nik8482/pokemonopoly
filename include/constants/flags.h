8e348d5cc1329da87835230109a89fdfc6cc2d91
Nikhil Modha <44924404+nik8482@users.noreply.github.com>
2025-01-10 20:38:10 +0000

    added battle scripts for abdi and neal

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1390,8 +1390,8 @@
 #define FLAG_HARSHI_MAD                             (SYSTEM_FLAGS + 0x21) // Set Harshi to be mad after telling story
 #define FLAG_REMOVE_KABI_1                          (SYSTEM_FLAGS + 0x22) // Remove kabi from abdi house
 #define EVENT_QUICK_CLAW_FOUND                      (SYSTEM_FLAGS + 0x23) // Unused Flag
-#define FLAG_UNUSED_0x884                           (SYSTEM_FLAGS + 0x24) // Unused Flag
-#define FLAG_UNUSED_0x885                           (SYSTEM_FLAGS + 0x25) // Unused Flag
+#define FLAG_ABDI_DEFEATED                           (SYSTEM_FLAGS + 0x24) // Unused Flag
+#define FLAG_NEAL_DEFEATED                           (SYSTEM_FLAGS + 0x25) // Unused Flag
 #define FLAG_UNUSED_0x886                           (SYSTEM_FLAGS + 0x26) // Unused Flag
 #define FLAG_UNUSED_0x887                           (SYSTEM_FLAGS + 0x27) // Unused Flag
 
